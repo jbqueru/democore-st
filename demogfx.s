5d49e27198e9db8362e076be979f59bde517d337
Jean-Baptiste Queru <jbqueru@gmail.com>
2020-08-05 01:01:57 -0700

    Very basic drawing code to use as a sample

--- demogfx.s
+++ demogfx.s
@@ -16,7 +16,29 @@
 
 update_thread_entry:
 ;;; Start customized code
-	nop
+	tst.l	front_drawn_data
+	beq.s	.new_config
+
+	move.l	most_recently_updated,a5
+	move.l	next_to_update,a6
+	move.w	(a5),d0
+	addq.w	#1,d0
+	cmp.w	#200,d0
+	bne.s	.y_in_range
+	moveq.l	#0,d0
+.y_in_range:
+	move.w	d0,(a6)
+
+	bra.s	.config_done
+
+.new_config:
+	move.l	#y_draw1,front_drawn_data
+	move.l	#y_draw2,front_to_draw_data
+	move.l	#y_draw3,back_drawn_data
+	move.l	#y_draw4,back_to_draw_data
+	move.l	back_to_draw_data,most_recently_updated
+	move.l	back_to_draw_data,next_to_update
+.config_done:
 ;;; End customized code
 
 	; Unblock draw thread, block this thread until it's ready again
@@ -29,7 +51,15 @@ update_thread_entry:
 
 draw_thread_entry:
 ;;; Start customized code
-	nop
+	move.l	back_buffer,a0
+	move.l	back_drawn_data,a5
+	move.l	back_to_draw_data,a6
+	move.w	(a5),d0
+	mulu.w	#160,d0
+	clr.w	(a0,d0.w)
+	move.w	(a6),d0
+	mulu.w	#160,d0
+	move.w	#$ffff,(a0,d0.w)
 ;;; End customized code
 
 	; Block this thread until it's ready again
@@ -43,6 +73,7 @@ draw_thread_entry:
 	bra.s	draw_thread_entry
 
 main_thread_entry:
+	move.w	#$707,$ffff8242.w
 main_loop:
 ;;; Start customized code
 	nop
@@ -53,3 +84,14 @@ main_loop:
 	cmp.b	#$39,$fffffc02.w
 	bne.s	main_loop
 	rts
+
+	.bss
+	.even
+y_draw1:
+	ds.w	1
+y_draw2:
+	ds.w	1
+y_draw3:
+	ds.w	1
+y_draw4:
+	ds.w	1
